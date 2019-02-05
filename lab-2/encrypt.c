a08fd5f2961795df5be913c600f8232599ba6af0
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-05 20:47:48 +0000

    Still stuck

--- lab-2/encrypt.c
+++ lab-2/encrypt.c
@@ -14,41 +14,54 @@ aes_gf28_t aes_gf28_add ( aes_gf28_t a, aes_gf28_t b );
 aes_gf28_t aes_gf28_mul ( aes_gf28_t a, aes_gf28_t b );
 aes_gf28_t xtime( aes_gf28_t a );
 aes_gf28_t sbox( aes_gf28_t a );
+void aes_enc_exp_step( aes_gf28_t* rk, aes_gf28_t rc );
+void aes_enc_rnd_key( aes_gf28_t* s, aes_gf28_t* rk );
 
 int main( int argc, char* argv[] ) {
-  uint8_t k[ 16 ] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
+  aes_gf28_t k[ 16 ] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
                       0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-  uint8_t m[ 16 ] = { 0x32, 0x43, 0xF6, 0xA8, 0x88, 0x5A, 0x30, 0x8D,
+  aes_gf28_t m[ 16 ] = { 0x32, 0x43, 0xF6, 0xA8, 0x88, 0x5A, 0x30, 0x8D,
                       0x31, 0x31, 0x98, 0xA2, 0xE0, 0x37, 0x07, 0x34 };
-  uint8_t c[ 16 ] = { 0x39, 0x25, 0x84, 0x1D, 0x02, 0xDC, 0x09, 0xFB,
-                      0xDC, 0x11, 0x85, 0x97, 0x19, 0x6A, 0x0B, 0x32 };
-  uint8_t t[ 16 ];
+  aes_gf28_t c[ 16 ] = { 0x39, 0x25, 0x84, 0x1D, 0x02, 0xDC, 0x09, 0xFB,
+                    0xDC, 0x11, 0x85, 0x97, 0x19, 0x6A, 0x0B, 0x32 };
+  aes_gf28_t t[ 16 ];
 
   AES_KEY rk;
+  
   // AES_set_encrypt_key( k, 128, &rk );
   // AES_encrypt( m, t, &rk );
-  for (int x = 0; x < 16; x++){
-    t[x] = m[x] ^ k[x];
-  }
+
+  aes_enc_rnd_key(m, k);
   printf("M xor K: ");
   for (int x = 0; x < 16; x++) {
-    printf("%x, ", t[x]);
+    printf("%x, ", m[x]);
   }
   printf("\n");
   for (int x = 0; x < 16; x++){
-    t[x] = sbox(t[x]);
+    t[x] = sbox(m[x]);
   }
   printf("S box: ");
   for (int x = 0; x < 16; x++) {
     printf("%x, ", t[x]);
   }
   printf("\n");
+
   // if( !memcmp( t, c, 16 * sizeof( uint8_t ) ) ) {
   //   printf( "AES.Enc( k, m ) == c\n" );
   // }
   // else {
   //   printf( "AES.Enc( k, m ) != c\n" );
   // }
+
+}
+
+void aes_enc_rnd_key( aes_gf28_t* s, aes_gf28_t* rk ) {
+  for (int x = 0; x < 16; x++){
+    s[x] = s[x] ^ rk[x];
+  }
+}
+
+void aes_enc_exp_step( aes_gf28_t* rk, aes_gf28_t rc ) {
 
 }
 
@@ -101,7 +114,7 @@ aes_gf28_t sbox( aes_gf28_t a ) {
 }
 
 aes_gf28_t xtime( aes_gf28_t a ) {
-  if (a & 0x80 == 0x80) {
+  if ((a & 0x80) == 0x80) {
     return 0x1B ^ ( a >> 1 );
   } else {
     return (a >> 1);
