3b6b6b405c8b0b80f25da7909ddbc7ea1f47635c
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-12 13:32:27 +0000

    Keygen needs implementing

---
+++ lab-3/rsa_gmp.c
@@ -0,0 +1,35 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+#include    <gmp.h>
+
+void rsa_keygen( mpz_t N, mpz_t e, mpz_t d, int lambda);
+void l2r_lexp( mpz_t r, mpz_t x, mpz_t y, mpz_t N);
+void rsa_enc( mpz_t c, mpz_t m, mpz_t e, mpz_t N);
+void rsa_dec( mpz_t m, mpz_t c, mpz_t d, mpz_t N);
+
+int main( int argc, char* argv[] ) {
+
+
+    return 0;
+}
+
+void rsa_keygen( mpz_t N, mpz_t e, mpz_t d, int lambda){
+
+
+}
+
+void l2r_lexp( mpz_t r, mpz_t x, mpz_t y, mpz_t N){
+
+
+}
+
+void rsa_enc( mpz_t c, mpz_t m, mpz_t e, mpz_t N){
+
+
+}
+
+void rsa_dec( mpz_t m, mpz_t c, mpz_t d, mpz_t N){
+
+
+}
